6a945d0cb43ab9a3ae3637e0b689e66742f0d71a
pisksand <pisksand@kkg-do.eu>
2022-11-15 10:16:49 +0100

    aaaa

--- src/DrillingMachine.java
+++ src/DrillingMachine.java
@@ -4,5 +4,16 @@ public class DrillingMachine {
     private int age;
     private double price;
     private boolean broken;
+
+    public DrillingMachine() {
+    name = "NR 1";
+    watt = (int)(Math.random()*1201+800);
+    age = (int)(Math.random()*11);
+    price = Math.random()*450 + 50;
+    }
+
+    public void showInfos(){
+        System.out.println(name + "-" + "Watt:" + watt + "-Preis:" + price);
+    }
 }
 
--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,5 +1,9 @@
 public class MainProgram {
     public static void main(String[] args) {
+    DrillingMachine e1 = new DrillingMachine();
+    DrillingMachine e2 = new DrillingMachine();
 
+    e1.showInfos();
+    e2.showInfos();
     }
 }
